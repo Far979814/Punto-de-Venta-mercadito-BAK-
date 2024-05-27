43ffb5c58bca565fecf84254c7e0f5e2faa3d75f
Far979814 <80871765+Far979814@users.noreply.github.com>
2024-05-26 21:21:00 -0600

    Update client.controllers.js

--- src/controllers/client.controllers.js
+++ src/controllers/client.controllers.js
@@ -7,4 +7,66 @@ export const getClients = async(req, res) =>{ //Obtener los clientes de la base.
     }catch (error){
         return res.status(500).json({message: error.message});
     }
+
+    // Para "employee":
+export const getEmployees = async (req, res) => {
+    try {
+        const [result] = await pool.query("SELECT * FROM employee");
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+// Para "product":
+export const getProducts = async (req, res) => {
+    try {
+        const [result] = await pool.query("SELECT * FROM product");
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+// Para "supplier":
+export const getSuppliers = async (req, res) => {
+    try {
+        const [result] = await pool.query("SELECT * FROM supplier");
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+// Para "user":
+export const getUsers = async (req, res) => {
+    try {
+        const [result] = await pool.query("SELECT * FROM user");
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+// Para "invoice":
+export const getInvoices = async (req, res) => {
+    try {
+        const [result] = await pool.query("SELECT * FROM invoice");
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+// Para "department":
+export const getDepartments = async (req, res) => {
+    try {
+        const [result] = await pool.query("SELECT * FROM department");
+        res.json(result);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+    
 }
